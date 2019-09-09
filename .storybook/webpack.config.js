be2ee9165d49944ce73de0002e7247c3b9c4f81d
Andreas Thomas <andreas.thomas@chronark.com>
2019-09-09 17:16:29 +0200

    circleci

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,12 +1,28 @@
 module.exports = ({ config }) => {
-  config.module.rules.push({
-    test: /\.(ts|tsx)$/,
-    use: [
-      {
-        loader: require.resolve("awesome-typescript-loader"),
-      },
-    ],
-  })
+  config.module.rules.push(
+    {
+      test: /\.(ts|tsx)$/,
+      use: [
+        {
+          loader: require.resolve("awesome-typescript-loader"),
+        },
+      ],
+    },
+    {
+      test: /\.css$/,
+      use: [
+        "style-loader",
+        "css-loader",
+        {
+          loader: "postcss-loader",
+          options: {
+            ident: "postcss",
+            plugins: [require("tailwindcss"), require("autoprefixer")],
+          },
+        },
+      ],
+    },
+  )
   config.resolve.extensions.push(".ts", ".tsx")
   return config
 }
