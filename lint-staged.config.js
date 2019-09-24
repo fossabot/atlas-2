c3239bb5a80c7ac9c05ec89ce5af07474ff8c6c3
Andreas Thomas <andreas93thomas@gmail.com>
2019-09-24 14:59:13 +0200

    removed unused packages

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,11 +1,9 @@
 module.exports = {
-  "package.json": ["npm run format-package-json", "git add"],
   "**/*.{css,gql,graphql,html,json,less,md,mdx,scss,vue,yaml,yml}": [
     "prettier --write",
     "git add",
   ],
   "**/*.{js,jsx,ts,tsx}": [
-    "import-sort --write",
     "prettier --write",
     "eslint --cache --ext '.js,.jsx,.ts,.tsx' --fix",
     "git add",
