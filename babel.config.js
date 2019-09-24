d078425c4b258734f4bb734075ee3566eb3082b2
Andreas Thomas <andreas93thomas@gmail.com>
2019-09-24 14:33:15 +0200

    ie11 and jest working together

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,21 @@
-module.exports = {
-  presets: [
-    "@babel/react",
-    "@babel/typescript",
-    [
-      "@babel/env",
-      {
-        modules: false,
-        targets: {
-          browsers: ["last 2 versions", "ie >= 11"],
+module.exports = api => {
+  const isTest = api.env("test")
+  api.cache(true)
+  return {
+    presets: [
+      "@babel/react",
+      "@babel/typescript",
+      [
+        "@babel/env",
+        {
+          modules: isTest ? "commonjs" : false,
+          targets: {
+            browsers: ["last 2 versions", "ie >= 11"],
+            node: "current",
+          },
         },
-      },
+      ],
     ],
-  ],
+    plugins: isTest ? ["macros"] : [],
+  }
 }
