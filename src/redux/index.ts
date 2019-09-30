23c497ac71d09c71b2dc1b30cb53453530fd9757
Andreas Thomas <andreas93thomas@gmail.com>
2019-09-30 17:08:23 +0200

    loading country data into redux

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -3,12 +3,12 @@ import { combineReducers } from "redux"
 import jobsReducer from "./jobs/reducer"
 import notificationReducer from "./notifications/reducer"
 import searchReducer from "./search/reducer"
-import countryReducer from "./countries/reducer"
+import countriesReducer from "./countries/reducer"
 const rootReducer = combineReducers({
   jobs: jobsReducer,
   notifications: notificationReducer,
   search: searchReducer,
-  countries: countryReducer,
+  countries: countriesReducer,
 })
 
 export type State = ReturnType<typeof rootReducer>
