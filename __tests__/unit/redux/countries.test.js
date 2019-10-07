a603ed6e271939efb3d42f2e77cde728398dce6b
Andreas Thomas <andreas.thomas@chronark.com>
2019-10-07 15:15:50 +0200

    added tests for redux

---
+++ __tests__/unit/redux/countries.test.js
@@ -0,0 +1,36 @@
+import { setSelectedCountries } from "../../../src/redux/countries/actions"
+import {
+  SET_SELECTED_COUNTRIES,
+  SetSelectedCountriesAction,
+} from "../../../src/redux/countries/types"
+import reducer from "../../../src/redux/countries/reducer"
+describe("actions", () => {
+  it("should create an action to set selected countries", () => {
+    const countries = ["USA", "GER", "ITA"]
+    const expectedAction = {
+      type: SET_SELECTED_COUNTRIES,
+      payload: countries,
+    }
+    expect(setSelectedCountries(countries)).toEqual(expectedAction)
+  })
+})
+
+describe("reducer", () => {
+  it("should return the initial state", () => {
+    expect(reducer(undefined, {})).toEqual({
+      selectedCountries: [],
+    })
+  })
+
+  it("should handle ADD_SELECTED_COUNTRIES", () => {
+    const countries = ["USA", "GER", "ITA"]
+    expect(
+      reducer([], {
+        type: SET_SELECTED_COUNTRIES,
+        payload: countries,
+      }),
+    ).toEqual({
+      selectedCountries: countries,
+    })
+  })
+})
