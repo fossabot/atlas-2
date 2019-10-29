b4d8ac53ac703e6fd81047029476d7107654df17
Andreas Thomas <andreas.thomas@chronark.com>
2019-10-29 12:24:26 +0100

    cleaned up countries and nominatim api

---
+++ src/lib/jobFilter.ts
@@ -0,0 +1,16 @@
+// import { Job } from "../types/customTypes"
+
+// export const filterJobsByGeometry = (
+//   jobs: Job[],
+//   geometry: Record<string, any>,
+// ): Job[] => {
+//   const filteredJobs: Job[] = []
+//   if (props.countries.selectedCountries.length === 0) {
+//     newShownJobs = props.jobs.allJobs
+//   } else {
+//     newShownJobs = getJobsInGeometry(
+//       props.jobs.allJobs,
+//       props.countries.selectedCountries,
+//     )
+//   }
+// }
