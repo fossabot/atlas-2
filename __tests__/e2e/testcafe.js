5c09ceb844993cac27683747bef49bccecabfe7f
Andreas Thomas <andreas.thomas@chronark.com>
2019-10-29 11:14:00 +0100

    cleaned up testcafe config

--- __tests__/e2e/testcafe.js
+++ __tests__/e2e/testcafe.js
@@ -7,31 +7,26 @@ let testcafe = null
 const browsers = process.env.CI
   ? ["chrome:headless"]
   : [
-      "chrome",
-      "firefox",
-      // "ie",
-      // "edge",
+      "all",
       "chrome:emulation:width=720;height=1280;mobile=true;orientation=vertical;touch=true",
       "chrome:emulation:width=1080;height=1920;mobile=true;orientation=vertical;touch=true",
     ]
+
+// Try to aim for 1 browser per thread.
+const concurrency = Math.max(1, Math.floor(browsers.length / os.cpus().length))
 
 createTestCafe("localhost", 1337, 1338)
   .then(tc => {
     testcafe = tc
     const runner = testcafe.createRunner()
 
-    return (
-      runner
-        .browsers(browsers)
-        // Try to aim for 1 browser per thread.
-        .concurrency(
-          Math.max(1, Math.floor(browsers.length / os.cpus().length)),
-        )
-        .reporter("spec")
-        .src("__tests__/e2e/*.test.ts")
-        .tsConfigPath("tsConfig.json")
-        .run()
-    )
+    return runner
+      .browsers(browsers)
+      .concurrency(concurrency)
+      .reporter("spec")
+      .src("__tests__/e2e/*.test.ts")
+      .tsConfigPath("tsconfig.json")
+      .run()
   })
   .then(() => {
     testcafe.close()
