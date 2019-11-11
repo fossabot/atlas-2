dd9293ae26ff11effc9080391afedf9124407656
Andreas Thomas <andreas.thomas@chronark.com>
2019-11-11 17:25:34 +0100

    removed simulated mobile browsers from testcafe

--- __tests__/e2e/testcafe.js
+++ __tests__/e2e/testcafe.js
@@ -6,9 +6,6 @@ let testcafe = null
 
 const browsers = process.env.CI ? ["chrome:headless"] : ["all"]
 
-// Try to aim for 1 browser per thread.
-const concurrency = Math.max(1, Math.floor((os.cpus().length - 1) / browsers.length))
-
 createTestCafe("localhost", 1337, 1338)
   .then(tc => {
     testcafe = tc
@@ -16,7 +13,7 @@ createTestCafe("localhost", 1337, 1338)
 
     return runner
       .browsers(browsers)
-      .concurrency(concurrency)
+      .concurrency(1)
       .reporter("spec")
       .src("__tests__/e2e/*.test.ts")
       .tsConfigPath("tsconfig.json")
