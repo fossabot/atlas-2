ecf17f890a0aacf05f290b24797c15236bf2322b
Andreas Thomas <andreas.thomas@chronark.com>
2019-11-11 17:19:10 +0100

    removed simulated mobile browsers from testcafe

--- __tests__/e2e/testcafe.js
+++ __tests__/e2e/testcafe.js
@@ -4,19 +4,10 @@ const os = require("os")
 
 let testcafe = null
 
-const browsers = process.env.CI
-  ? ["chrome:headless"]
-  : [
-      "all",
-      "chrome:emulation:width=720;height=1280;mobile=true;orientation=vertical;touch=true",
-      "chrome:emulation:width=1080;height=1920;mobile=true;orientation=vertical;touch=true",
-    ]
+const browsers = process.env.CI ? ["chrome:headless"] : ["all"]
 
 // Try to aim for 1 browser per thread.
-const concurrency = Math.max(
-  1,
-  Math.floor((os.cpus().length - 1) / browsers.length),
-)
+const concurrency = Math.max(1, Math.floor((os.cpus().length - 1) / browsers.length))
 
 createTestCafe("localhost", 1337, 1338)
   .then(tc => {
