04a1c2fe779f76d6433a56738e053945ea6e97cc
andreas <andreas@pop-os.localdomain>
2019-11-14 15:15:56 +0100

    added logrocket and sentry

--- src/lib/index.tsx
+++ src/lib/index.tsx
@@ -2,13 +2,17 @@ import "../../static/css/ol-ext.css"
 import "../../static/css/ol.css"
 import "../../static/css/tailwind.css"
 
+import * as Sentry from "@sentry/browser"
+
 import React from "react"
 import ReactDOM from "react-dom"
 import { Provider } from "react-redux"
 
 import App from "../components/App"
 import store from "../redux/store"
-
+import LogRocket from "logrocket"
+LogRocket.init("1eugfg/atlas")
+Sentry.init({ dsn: "https://6bf6e05112f444059aadd9afe4dc03fd@sentry.io/1820422", release: "atlas@0.0.1" })
 ReactDOM.render(
   <Provider store={store}>
     <App />
