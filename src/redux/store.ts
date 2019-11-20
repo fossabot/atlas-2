d8a960484fe93ae47f1c351e2992dad6e88bb97d
Andreas Thomas <andreas.thomas@chronark.com>
2019-11-20 15:53:38 +0100

    no longer using logrocket

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,11 @@
 import { applyMiddleware, createStore } from "redux"
+import { composeWithDevTools } from "redux-devtools-extension"
 import thunk from "redux-thunk"
-
-import LogRocket from "logrocket"
 
 import rootReducer from "."
 
 const initialState = {}
 
-const store = createStore(rootReducer, initialState, applyMiddleware(thunk, LogRocket.reduxMiddleware()))
+const store = createStore(rootReducer, initialState, composeWithDevTools(applyMiddleware(thunk)))
 
 export default store
