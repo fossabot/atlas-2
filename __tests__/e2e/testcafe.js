6fb4249e25b787c494214565bd199e5ee5c945a3
Andreas Thomas <andreas.thomas@chronark.com>
2019-11-20 15:51:34 +0100

    added quick IE11 only test

--- __tests__/e2e/testcafe.js
+++ __tests__/e2e/testcafe.js
@@ -1,11 +1,13 @@
 // @ts-ignore
 const createTestCafe = require("testcafe")
-const os = require("os")
 
 let testcafe = null
+const concurrency = 1
 
-const browsers = process.env.CI ? ["chrome:headless"] : ["all"]
-
+let browsers = process.env.CI ? ["chrome:headless"] : ["all"]
+if (process.env.IE_ONLY) {
+  browsers = ["ie"]
+}
 createTestCafe("localhost", 1337, 1338)
   .then(tc => {
     testcafe = tc
@@ -14,10 +16,11 @@ createTestCafe("localhost", 1337, 1338)
     return runner
       .browsers(browsers)
       .concurrency(1)
-      .reporter("spec")
       .src("__tests__/e2e/*.test.ts")
       .tsConfigPath("tsconfig.json")
-      .run()
+      .run({
+        stopOnFirstfail: true,
+      })
   })
   .then(() => {
     testcafe.close()
