ea65806a20edc51e4a12efadb0ffba8888038902
Andreas Thomas <andreas.thomas@chronark.com>
2019-12-05 13:38:44 +0100

    added http to url

--- src/lib/mapbox.ts
+++ src/lib/mapbox.ts
@@ -1,4 +1,4 @@
-const serverURL = "http://jbs-osm.informatik.fh-nuernberg.de:52000"
+const serverURL = "http://localhost:52000"
 
 export const mapbox = {
   tiles: serverURL + "/tiles?x={x}&y={y}&z={z}",
