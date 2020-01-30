e105531e1c10d40f7f0a30dcfbd542643bb3bc31
Andreas Thomas <andreas@jbs-osm.informatik.fh-nuernberg.de>
2020-01-30 15:38:13 +0100

    refactored charon api

--- src/lib/apis/charon.ts
+++ src/lib/apis/charon.ts
@@ -1,12 +1,9 @@
-/**
- *
- */
 import { GeocodingResponseObject } from "../../types/customTypes"
 import Axios from "axios"
 export default class Charon {
   private serverURL: string
   public constructor() {
-    this.serverURL = "http://localhost:52000"
+    this.serverURL = "http://jbs-osm.informatik.fh-nuernberg.de:52000"
   }
 
   public getTileURL(): string {
@@ -17,34 +14,18 @@ export default class Charon {
     return fetch(this.serverURL + "/style").then(r => r.json())
   }
 
-  public async forwardGeocoding(search: string): Promise<any | undefined> {
-    const url = this.serverURL + `/geocoding/forward/?query=${search}`
-
-    const response = await fetch(url, {
-      mode: "no-cors",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
+  public async forwardGeocoding(query: string): Promise<Record<string, any> | undefined> {
+    const response = await Axios.get(this.serverURL + `/geocoding/forward/?query=${query}`)
     if (response.status === 200) {
-      const json = await response.json()
-      return {
-        lat: Number(json[0].lat),
-        lon: Number(json[0].lon),
-        geojson: json[0].geojson,
-      }
+      return response.data
     }
-    return undefined
   }
 
   public async reverseGeocoding(lat: number, lon: number): Promise<GeocodingResponseObject | undefined> {
     const url = this.serverURL + `/geocoding/reverse/?lat=${lat}&lon=${lon}`
-    console.log(url)
     const response = await Axios.get(url)
     if (response.status === 200) {
-      console.log(response)
       const json = response.data
-      console.log(json)
       return json
     }
     return undefined
